d561bc8e41d49a6e16a83dda8f143c12fe429cad
Akshay Kumar <imakshay13@gmail.com>
2022-02-20 17:12:18 +0530

    Updated Feed and Pin

--- memories_frontend/src/container/Home.jsx
+++ memories_frontend/src/container/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
 
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
-    console.log(query);
+
     client.fetch(query).then((data) => {
       setUser(data[0]);
     });
